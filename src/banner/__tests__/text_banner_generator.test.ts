a1144e4d9bc5d929a69f3481eff1aef1b445e0db
alator21 <botazz133@gmail.com>
2024-12-07 14:23:59 +0200

    Use markdown to render the banner

--- src/banner/__tests__/text_banner_generator.test.ts
+++ src/banner/__tests__/text_banner_generator.test.ts
@@ -7,11 +7,19 @@ describe("text_banner_generator", () => {
 		initializeLastFmApi(process.env.API_KEY!);
 	});
 
-	test("creates simple bannerr", async () => {
+	test("creates simple banner", async () => {
 		const tracks = await Bun.file('src/banner/__tests__/mock-tracks.json').json();
 		// const tracks = await getTopTracks({ user: 'alator21' });
 
 		const banner = await generateTextBanner({ tracks: tracks.toptracks.track, style: 'default' });
+		console.log(banner);
+	});
+
+	test("creates experimental banner", async () => {
+		const tracks = await Bun.file('src/banner/__tests__/mock-tracks.json').json();
+		// const tracks = await getTopTracks({ user: 'alator21' });
+
+		const banner = await generateTextBanner({ tracks: tracks.toptracks.track, style: 'experimental' });
 		console.log(banner);
 	});
 
--- src/banner/text_banner_generator.ts
+++ src/banner/text_banner_generator.ts
@@ -1,36 +1,37 @@
 import type { GetTopTracksResponse } from "../lastfm/user/getTopTracks";
 
+type Track = GetTopTracksResponse['toptracks']['track'][0];
 type GenerateTextBannerRequest = {
-	tracks: Array<GetTopTracksResponse['toptracks']['track'][0]>;
+	tracks: Array<Track>;
 	style: 'default' | 'experimental';
 };
 
 
 /**
- * returns the top tracks of the week in text format
+ * returns the top tracks of the week in markdown format
  */
 export async function generateTextBanner(request: GenerateTextBannerRequest): Promise<string> {
-	const { tracks, style } = request;
+	const { tracks: allTracks, style } = request;
+	const tracks = allTracks.slice(0, 5);
 	if (style === 'default') {
-		return defaultStyle(tracks.slice(0, 5));
+		return defaultStyle(tracks);
 	}
 	return experimentalStyle(tracks);
 }
 
 
-async function defaultStyle(tracks: Array<GetTopTracksResponse['toptracks']['track'][0]>): Promise<string> {
-	let formattedText = `My top tracks this week!\n`;
-	formattedText += `-------------------------------\n`;
+async function defaultStyle(tracks: Array<Track>): Promise<string> {
+	let formattedText = `## My top tracks this week!\n`;
 
 	for (let i = 0; i < tracks.length; i++) {
 		const track = tracks[i];
 		const index = i + 1;
-		formattedText += `${index} - ${track.name} by ${track.artist.name} played ${track.playcount} times\n`;
+		formattedText += `${index}. **${track.name}** by **${track.artist.name}** played **${track.playcount}** times\n`;
 	}
 
 	return formattedText;
 }
-async function experimentalStyle(tracks: Array<GetTopTracksResponse['toptracks']['track'][0]>): Promise<string> {
+async function experimentalStyle(tracks: Array<Track>): Promise<string> {
 	throw new Error(`not implemented yet`);
 }
 
