df3adda917aa6ad33288420ccdd4891d88756d29
alator21 <botazz133@gmail.com>
2024-12-12 23:18:34 +0200

    Create test-data module

--- src/banner/__tests__/text_banner_generator.test.ts
+++ src/banner/__tests__/text_banner_generator.test.ts
@@ -1,6 +1,7 @@
 import { test, describe, beforeAll } from "bun:test";
 import { initializeLastFmApi } from "../../lastfm/api";
 import { generateTextBanner } from "../text_banner_generator";
+import { getTopTracks } from "../../lastfm/user/getTopTracks";
 
 describe("text_banner_generator", () => {
 	beforeAll(() => {
@@ -8,14 +9,14 @@ describe("text_banner_generator", () => {
 	});
 
 	test("creates simple banner", async () => {
-		const tracks = await Bun.file('src/banner/__tests__/mock-tracks.json').json();
+		const tracks = await Bun.file('src/test-data/user/getTopTracks.json').json();
 		// const tracks = await getTopTracks({ user: 'alator21' });
 
 		const banner = await generateTextBanner({ tracks: tracks.toptracks.track, period: '7day', style: 'default' });
 		console.log(banner);
 	});
 
-	test("creates experimental banner", async () => {
+	test.skip("creates experimental banner", async () => {
 		const tracks = await Bun.file('src/banner/__tests__/mock-tracks.json').json();
 		// const tracks = await getTopTracks({ user: 'alator21' });
 
