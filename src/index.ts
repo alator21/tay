e54ba6415f919d4ad30dc7611526f54c2b26eec9
alator21 <botazz133@gmail.com>
2024-12-12 23:19:15 +0200

    Expose get top artists

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export { initializeLastFmApi } from './lastfm/api.ts';
 export { getTopTracks } from './lastfm/user/getTopTracks.ts'
+export { getTopArtists } from './lastfm/user/getTopArtists.ts'
 export { generateTextBanner } from './banner/text_banner_generator.ts'
 // export { generateImageBanner } from './banner/image_banner_generator.ts'
