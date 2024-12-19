f1ebe7dfa63af85ea71dd955dd77c88b4e3874f7
alator21 <botazz133@gmail.com>
2024-12-19 20:24:37 +0200

    Expose new functionality to consumers

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 export { initializeLastFmApi } from './lastfm/api.ts';
+export { initializeMusicBrainzApi } from './musicbrainz/api.ts'
 export { getTopTracks } from './lastfm/user/getTopTracks.ts'
+export { enrichTracks } from './extra-metadata/enrichTracks.ts'
 export { getTopArtists } from './lastfm/user/getTopArtists.ts'
 export { generateTextBanner } from './banner/text_banner_generator.ts'
-// export { generateImageBanner } from './banner/image_banner_generator.ts'
+export { generateImageBanner } from './banner/image_banner_generator.ts'
